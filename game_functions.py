4c2a657803a5d1f6ba22452b527d1c09adac8249
luxihua201010028 <42412379+luxihua201010028@users.noreply.github.com>
2021-05-21 23:35:41 +0800

    Limit the range of motion of the ship Refactoring check_ Event function

--- game_functions.py
+++ game_functions.py
@@ -2,6 +2,20 @@ import sys
 import pygame
 
 
+def check_keydown_event(event, ship):
+    if event.key == pygame.K_RIGHT:  # 检测到是向右的键
+        ship.moving_right = True
+    if event.key == pygame.K_LEFT:  # 检测到是向左的键
+        ship.moving_left = True
+
+
+def check_keyup_event(event, ship):
+    if event.key == pygame.K_RIGHT:  # 检测到是向右的键
+        ship.moving_right = False
+    if event.key == pygame.K_LEFT:  # 检测到是向左的键
+        ship.moving_left = False
+
+
 def check_events(ship):
     # 监视键盘和鼠标状态
     for event in pygame.event.get():
@@ -9,16 +23,10 @@ def check_events(ship):
         if event.type == pygame.QUIT:
             # 退出
             sys.exit()
-        elif event.type == pygame.KEYDOWN:  # 检测到键盘事件,按下
-            if event.key == pygame.K_RIGHT:  # 检测到是向右的键
-                ship.moving_right = True
-            if event.key == pygame.K_LEFT:  # 检测到是向左的键
-                ship.moving_left = True
-        elif event.type == pygame.KEYUP:  # 检测到键盘事件,松开
-            if event.key == pygame.K_RIGHT:  # 检测到是向右的键
-                ship.moving_right = False
-            if event.key == pygame.K_LEFT:  # 检测到是向左的键
-                ship.moving_left = False
+        elif event.type == pygame.KEYDOWN:  # 响应按键
+            check_keydown_event(event, ship)
+        elif event.type == pygame.KEYUP:  # 响应松开
+            check_keyup_event(event, ship)
 
 
 def update_screen(ai_settings, screen, ship, back_ground_image):
