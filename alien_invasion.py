ce10681dbdce3dbe88232d39a59c8fe2e97289bf
luxihua201010028 <42412379+luxihua201010028@users.noreply.github.com>
2021-05-21 23:01:12 +0800

    the ship can move to left and right

--- alien_invasion.py
+++ alien_invasion.py
@@ -20,6 +20,7 @@ def run_game():
     # 开始游戏主循环
     while True:
         gf.check_events(ship)
+        ship.update()
         gf.update_screen(ai_settings, screen, ship, back_ground_image)
 
 
--- ship.py
+++ ship.py
@@ -15,7 +15,14 @@ class Ship:
         # 将每艘新飞船放在屏幕底部中央
         self.rect.centerx = self.screen_rect.centerx  # 飞船中心的x坐标
         self.rect.bottom = self.screen_rect.bottom  # 飞船下边缘的y坐标
-
+        self.moving_right=False
+        self.moving_left=False
+    def update(self):
+        '''根据移动标志调整飞船位置'''
+        if self.moving_right:
+            self.rect.centerx+=1
+        if self.moving_left:
+            self.rect.centerx-=1
     def blitme(self):
         """在指定的位置绘制飞船"""
         self.screen.blit(self.image, self.rect)
